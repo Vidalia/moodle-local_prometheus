9e5632a0739fe08d5d357268a92e9c8aa1af62e7
John Maydew <jdmayd@essex.ac.uk>
2023-11-27 12:13:22 +0000

    Bumped version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023111400;
+$plugin->version   = 2023112700;
 $plugin->requires  = 2019051100;
 $plugin->component = 'local_prometheus';
-$plugin->release   = 'v1.1.2';
+$plugin->release   = 'v1.1.3';
